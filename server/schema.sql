28b3c1b5259c731f72044ce12eabb234ba00a2f7
naomiajacobs <naomiajacobs@gmail.com>
2015-09-11 16:06:26 -0700

    implement database schema

--- server/schema.sql
+++ server/schema.sql
@@ -3,14 +3,26 @@ CREATE DATABASE chat;
 USE chat;
 
 CREATE TABLE messages (
-  /* Describe your table here.*/
+  id int(3) NOT NULL auto_increment,
+  message varchar(140) NOT NULL,
+  user int(3) NOT NULL,
+  room int(3) NOT NULL,
+  PRIMARY KEY(id)
 );
 
 /* Create other tables and define schemas for them here! */
 
+CREATE TABLE users (
+  id int(3) NOT NULL auto_increment,
+  username varchar(30) NOT NULL,
+  PRIMARY KEY(id)
+);
 
-
-
+CREATE TABLE rooms (
+  id int(3) NOT NULL auto_increment,
+  roomname varchar(30) NOT NULL,
+  PRIMARY KEY(id)
+);
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
  *  to create the database and the tables.*/
